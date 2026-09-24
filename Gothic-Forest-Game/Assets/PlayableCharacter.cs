807176056e0438bc4edef90f7be791e0620a4f50
OlaKh <pilpilola@gmail.com>
2026-09-24 22:46:15 +0300

    PlayableCharacter class- skeleton
    Only a basic skeleton of the class, everything commented out

---
+++ Gothic-Forest-Game/Assets/PlayableCharacter.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+abstract class PlayableCharacter : MonoBehaviour : IDamagable
+{
+    /*
+    // PlayableCharacter Variables / Ola 24.9.26
+    [SerializeField] protected int speed;
+    [SerializeField] protected int maxHp;
+    protected int currentHp;
+    
+    
+    ApplyDamage(IDamagable)
+    SpecialAbility()
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //set hp and maybe speed to default state?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement()
+    }
+
+    void Movement()
+    {
+        // Place holder for movment - using speed, decide on needed movement
+    }
+
+    ApplyDamage(IDamagable)
+    {
+        // Place holder
+    }
+
+    ApplyDamage(IDamagable)
+    {
+        // Place holder
+    }
+
+    SpecialAbility()
+     {
+        // Place holder
+    }   
+
+
+    */
+}
