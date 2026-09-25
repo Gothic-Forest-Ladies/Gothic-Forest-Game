1e6845fc1e9e4835ceb21215e16670ec943d0da8
OlaKh <pilpilola@gmail.com>
2026-09-25 22:21:28 +0300

    Some architecture fixes or something

---
+++ Assets/Scripts/IDamagable/Playable/Players/Player1.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Player1 : PlayableCharacter
+{
+ 
+    // // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    override public void Movement()
+    {
+
+    }
+}
